11b71d0ccd9dcac48e72b5e3b952b9b8ab936b2c
AneeqahJ <aneeqahjones2@gmail.com>
2021-07-13 16:08:02 +0200

    Added File

--- app1.py
+++ app1.py
@@ -24,9 +24,9 @@ def admin(name):
 @app.route('/moola/<int:salary>')
 def payment(salary):
     if salary > 10000:
-        return redirect("https://www.sahomeloans.com/")
+        return "Baller Alert"
     else:
-        return redirect("http://www.fnb.co.za")
+        return redirect("https://www.sahomeloans.com/")
 
 
 if __name__ == '__main__':
